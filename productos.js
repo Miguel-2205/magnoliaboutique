dcb76c814835a56ae016aede59937e84f0da840e
Miguel-2205 <miguel.chirino.parra@gmail.com>
2026-08-22 15:59:49 -0300

    Add files via upload

--- productos.js
+++ productos.js
@@ -1,38 +1,14 @@
 const listaProductos = [
     // --- DESTACADOS (puedes marcar con true los que quieras que resalten) ---
     {
-        categoria: "camperas",
-        nombre: "Campera Puffer",
-        talles: "TS, TM, TL, TXL",
-        precio: "$44.000",
-        destacado: false, // <-- Esto indica que es destacado
-        imagenes: [
-            "img/abrigos/puffer-frente.jpeg",
-            "img/abrigos/puffer-espalda.jpeg"
-        ]
-    },
-    {
-        categoria: "jeans",
-        nombre: "Chupin con Brillo",
-        talles: "Del 38 al 50",
-        precio: "$38.000",
-        destacado: true, // <-- Esto indica que es destacado
-        imagenes: [
-            "img/chupin-brillo/frente.jpeg",
-            "img/chupin-brillo/perfil.jpeg",
-            "img/chupin-brillo/espalda.jpeg"
-        ]
-    },
-    // Resto de los productos normales...
-    {
         categoria: "accesorios",
-        nombre: "Accesorio de Ejemplo",
-        talles: "Único",
-        precio: "$5.000",
+        nombre: "Cintos",
+        talles: "Varios Modelos",
+        precio: "$12.000",
         destacado: false,
-        imagenes: ["img/default.jpeg"]
+        imagenes: ["img/accesorios/cinto/1.jpg",]
     },
-    {
+     {
         categoria: "buzos",
         nombre: "Buzo Oversize",
         talles: "S, M, L",
@@ -40,7 +16,7 @@ const listaProductos = [
         destacado: false,
         imagenes: ["img/default.jpeg"]
     },
-    {
+     {
         categoria: "calsas",
         nombre: "Calsa Deportiva",
         talles: "1, 2, 3",
@@ -49,14 +25,51 @@ const listaProductos = [
         imagenes: ["img/default.jpeg"]
     },
     {
+        categoria: "camperas",
+        nombre: "Campera Plush",
+        talles: "T3, T6",
+        precio: "$20.000",
+        destacado: false, // <-- Esto indica que es destacado
+        imagenes: [
+            "img/camperas/plush/1.jpg",
+            "img/camperas/plush/2.jpg",
+            "img/camperas/plush/3.jpg",
+            "img/camperas/plush/4.jpg"
+        ]
+    },
+    {
+        categoria: "camperas",
+        nombre: "Jeans con Tachas",
+        talles: "T1, T3, T6",
+        precio: "$30.000",
+        destacado: false, // <-- Esto indica que es destacado
+        imagenes: [
+            "img/camperas/tachas/1.jpg",
+            "img/camperas/tachas/2.jpg"
+        ]
+    },
+    {
+        categoria: "chaleco",
+        nombre: "Scarlet",
+        talles: "T2, T3",
+        precio: "$32.000",
+        destacado: false, // <-- Esto indica que es destacado
+        imagenes: [
+            "img/chaleco/scarlet/1.png",
+            "img/chaleco/scarlet/2.jpg"
+        ]
+    },
+    {
         categoria: "chaleco",
         nombre: "Chaleco Puffer",
         talles: "TM, TL, XL",
         precio: "$30.000",
         destacado: false,
-        imagenes: ["img/abrigos/chaleco-frente.jpeg"]
+        imagenes: ["img/chaleco/puffer/1.jpg",
+            "img/chaleco/puffer/2.jpg"
+        ]
     },
-    {
+      {
         categoria: "conjuntos",
         nombre: "Conjunto Urbano",
         talles: "S, M",
@@ -66,11 +79,24 @@ const listaProductos = [
     },
     {
         categoria: "jeans",
+        nombre: "Chupin con Brillo",
+        talles: "Del 38 al 50",
+        precio: "$38.000",
+        destacado: true, // <-- Esto indica que es destacado
+        imagenes: [
+            "img/jeans/chupin-brillo/frente.jpeg",
+            "img/jeans/chupin-brillo/perfil.jpeg",
+            "img/jeans/chupin-brillo/espalda.jpeg",
+            "img/jeans/chupin-brillo/detalle.jpeg",
+        ]
+    },
+    {
+        categoria: "jeans",
         nombre: "Chupin Azul",
         talles: "38, 40, 42, 44, 46",
         precio: "$20.000",
         destacado: false,
-        imagenes: ["img/pantalones/chupin-azul.jpeg"]
+        imagenes: ["img/jeans/chupin-azul.jpeg"]
     },
     {
         categoria: "jogging",
@@ -98,11 +124,39 @@ const listaProductos = [
     },
     {
         categoria: "remeras",
-        nombre: "Básica Manga Larga",
+        nombre: "Manga Larga",
         talles: "L, 2XL",
         precio: "$8.000",
         destacado: false,
-        imagenes: ["img/remeras/manga-larga.jpeg"]
+        imagenes: ["img/remeras/basica-larga/2.png",
+            "img/remeras/basica-larga/1.png",
+            "img/remeras/basica-larga/3.png",
+            "img/remeras/basica-larga/4.png",
+            "img/remeras/basica-larga/5.jpg"
+        ]
+    },
+     {
+        categoria: "remeras",
+        nombre: "Manga Princesa",
+        talles: "T3, T6",
+        precio: "$12.000",
+        destacado: false,
+        imagenes: ["img/remeras/manga-princesa/1.png",
+            "img/remeras/manga-princesa/2.png",
+            "img/remeras/manga-princesa/3.png",
+            "img/remeras/manga-princesa/4.jpg"
+        ]
+    },
+    {
+        categoria: "remeras",
+        nombre: "Body con Cierre",
+        talles: "Talle Unico",
+        precio: "$8.500",
+        destacado: false,
+        imagenes: ["img/remeras/body-cierre/frente.png",
+            "img/remeras/body-cierre/perfil.png",
+            "img/remeras/body-cierre/espalda.png"
+        ]
     },
     {
         categoria: "vestido",
